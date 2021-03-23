c511f07c342f7c65f82dd0848ffff37f0b7de4b1
rulanr <79989441+rulanr@users.noreply.github.com>
2021-03-23 23:08:59 +0700

    Add files via upload

---
+++ Minggu 3/Praktek/inputbobo.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Game HP Favorit ~ Input Combobox</title>
+</head>
+<body>
+
+    <FORM ACTION="prosescombobox.php" METHOD="POST" NAME="input">
+        <h2>Pilih Game HP Favorit anda:</h2>
+            <select name="kartun">
+            <option value="Free Fire">Free Fire</option>
+            <option value="Mobile Legend">Mobile Legend</option>
+            <option value="PES 2021">PES 2021</option>
+            <option value="PUBG">PUBG</option>
+            <option value="Clash of Clans">Clash of Clans</option>
+            </select>
+        <input type="submit" name="Pilih" value="Pilih">
+    </FORM>
+</body>
+</html>
