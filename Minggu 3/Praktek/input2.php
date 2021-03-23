0d884ced169cfdf076308e2a7f26686426c2bc3b
rulanr <79989441+rulanr@users.noreply.github.com>
2021-03-23 23:08:22 +0700

    Add files via upload

---
+++ Minggu 3/Praktek/input2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title> Form </title>
+</head>
+
+<body>
+    <form action ="proses02.php" method="post" name="input">
+        Nama : <input type="text" name="nama">
+        <br>
+        <input type="submit" name="Input" value="Input">
+    </form>
+</body>
+</html>
