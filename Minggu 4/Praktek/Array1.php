75bed5f759aebf38df186d3743919767b69593fb
rulanr <79989441+rulanr@users.noreply.github.com>
2021-03-30 23:27:46 +0700

    Create Array1.php

---
+++ Minggu 4/Praktek/Array1.php
@@ -0,0 +1,12 @@
+<?php
+$arrHewan = array ("Singa", "Paus", "Buaya", "Merpati");
+echo $arrHewan[0]; //Singa
+echo $arrHewan[3]; //Merpati
+$arrWarna = array();
+$arrWarna[] = "Coklat";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+echo $arrWarna[0]; //Coklat
+echo $arrWarna[2]; //Hijau
+?>
